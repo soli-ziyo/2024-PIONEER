4e16082567c1edb01e9186f0546e6ee1402cf0f0
mins107 <mins@ewha.ac.kr>
2024-07-23 19:37:46 +0900

    초기설정 2

---
+++ stew/src/components/Issue.jsx
@@ -0,0 +1,5 @@
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import axios from "axios";
+
+//관심사 변경 컴포넌트
---
+++ stew/src/pages/ProfilePage.jsx
@@ -0,0 +1,6 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+
+
+//설정-프로필 수정 페이지
