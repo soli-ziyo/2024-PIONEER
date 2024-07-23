d6f0060b956668d0a4d888082feb9669fbcec507
mins107 <mins@ewha.ac.kr>
2024-07-23 19:12:27 +0900

    초기설정

---
+++ stew/stew/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+import HomePage from "./pages/HomePage";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ stew/stew/src/components/Write.jsx
@@ -0,0 +1,5 @@
+import { useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import axios from "axios";
+
+//게시글 작성 컴포넌트 (임시)
