87bb8cf4565dd29506a26aabce7185734c48afba
soli-ziyo <solac@ewha.ac.kr>
2024-08-01 19:42:49 +0900

    (settings:local 설정) -> state/edit/ 3번 항목 수정

--- DRF/state/serializers.py
+++ DRF/state/serializers.py
@@ -5,7 +5,7 @@ class StateEditSerializer(serializers.ModelSerializer):
     user_id = serializers.IntegerField(source='user.id', read_only=True)
     state_id = serializers.IntegerField(source='id', read_only=True)
     nickname = serializers.CharField(source='user.nickname', read_only=True)
-    profile = serializers.ImageField(source='user.profile', read_only=True)
+    profile = serializers.ImageField(source='user.profile', required=False)
     phonenum = serializers.IntegerField(source='user.phonenum', read_only=True)
 
     class Meta:
