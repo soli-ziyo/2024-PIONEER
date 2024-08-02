acdcdd37aec6201b24af8cc730371e226e1b4536
soli-ziyo <solac@ewha.ac.kr>
2024-08-02 23:40:01 +0900

    8/2 배포준비완료 (userprofile시리얼라이저에 user_id 추가)

--- DRF/drfproject/settings.py
+++ DRF/drfproject/settings.py
@@ -24,6 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uaorw&9e@82x=&oend)h%3b2wnw96-b9u3mi0&2zx!@^5uck-('
 
 # SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 #DEBUG = False
 
