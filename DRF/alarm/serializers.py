c14592a1fae774f75c7f07552461b85d4f50f0fb
soli-ziyo <solac@ewha.ac.kr>
2024-08-05 00:51:23 +0900

    alarm 삭제

--- DRF/alarm/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import *
-
-class AlarmSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Alarm
-        fields = ['change_interest', 'family_interest_update', 'family_hashtag_update', 'family_comment']
