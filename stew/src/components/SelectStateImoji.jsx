0c88f213335d2628a8359e247d095d9a2d443b49
s-uxun <serenasol@ewha.ac.kr>
2024-08-06 08:15:38 +0900

    Design: 모달 이미지 수정

--- stew/src/components/SelectStateImoji.jsx
+++ stew/src/components/SelectStateImoji.jsx
@@ -101,13 +101,12 @@ const EmojiText = styled.div`
 `;
 
 const EmojiGrid = styled.div`
+  padding: 0px 30px;
   display: grid;
   align-items: center;
   justify-content: center;
   grid-template-columns: repeat(3, 1fr);
   aspect-ratio: 1;
-  height: 70%;
-  width: 70%;
   max-width: 390px;
 `;
 
