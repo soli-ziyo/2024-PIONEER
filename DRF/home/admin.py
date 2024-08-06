d73123b7c59ecc0e6ee0c321770a52d2a055849e
s-uxun <164325907+s-uxun@users.noreply.github.com>
2024-08-06 23:12:32 +0900

    Delete DRF directory

--- DRF/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from home.models import *
-
-# Register your models here.
-
-admin.site.register(HashTag)
-admin.site.register(WeekHashTag)
-#admin.site.register(Home)
